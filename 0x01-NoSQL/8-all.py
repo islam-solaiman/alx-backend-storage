a14f48e1ebc43b13d4c81c7c076c3c6341d013f4
Islam Solaiman <islam.solaiman92@gmail.com>
2024-01-23 20:10:47 +0000

    Task 8. List all documents in Python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,5 +1,7 @@
-// Python function that lists all documents in a collection
-''' Task 8's module. '''
+#!/usr/bin/env python3
+'''
+    Python function that lists all documents in a collection
+'''
 
 
 def list_all(mongo_collection):
